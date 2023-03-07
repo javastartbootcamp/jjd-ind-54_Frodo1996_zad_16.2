5c1ab07a159c25eaae036a5df197373a2105f301
Krzysiek Przystarz <frodson35@gmail.com>
2023-03-07 21:05:31 +0100

    Niestety nie mam już pomysłów jak doprowadzić kod do takiego stanu, by przechodziły wszystkie testy.. Próbowałem, lecz nie znalazłem rozwiązania, mam jeszcze sporo czasu na rozwiązanie go, także może uda się Wam przekazać mi jakieś pomocne sugestie :D

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,5 +1,11 @@
 package pl.javastart.task;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.TemporalAccessor;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +16,47 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
-        // uzupełnij rozwiązanie. Korzystaj z przekazanego w parametrze scannera
+        DateTimeFormatter formatter = getDateTimeFormatter();
+        System.out.println("Podaj datę:");
+        String userDate = scanner.nextLine();
+        TemporalAccessor parse = formatter.parse(userDate);
+        LocalDateTime localTime = LocalDateTime.from(parse);
+        printDateAndTime(formatter, localTime);
     }
 
+    private static DateTimeFormatter getDateTimeFormatter() {
+        return new DateTimeFormatterBuilder()
+                .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+                .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                .appendOptional(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"))
+                .toFormatter();
+    }
+
+    LocalDateTime convertToUtc(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime();
+    }
+
+    LocalDateTime convertToLondon(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("Europe/London")).toLocalDateTime();
+    }
+
+    LocalDateTime convertToLosAngeles(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("America/Los_Angeles")).toLocalDateTime();
+    }
+
+    LocalDateTime convertToSydney(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("Australia/Sydney")).toLocalDateTime();
+    }
+
+    private void printDateAndTime(DateTimeFormatter datePattern, LocalDateTime localTime) {
+        System.out.println("Czas lokalny: " + localTime.format(datePattern));
+        LocalDateTime utc = convertToUtc(localTime);
+        System.out.println("UTC: " + utc.format(datePattern));
+        LocalDateTime london = convertToLondon(localTime);
+        System.out.println("Londyn: " + london.format(datePattern));
+        LocalDateTime losAngeles = convertToLosAngeles(localTime);
+        System.out.println("Los Angeles: " + losAngeles.format(datePattern));
+        LocalDateTime sydney = convertToSydney(localTime);
+        System.out.println("Sydney: " + sydney.format(datePattern));
+    }
 }
